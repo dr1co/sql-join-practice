8de4cecdc724e1b29fbfed2fe0ad9e54ba7bfe0f
dr1co <dricomatoba@hotmail.com>
2022-07-26 20:55:19 -0300

    solved bonus question

--- answers.sql
+++ answers.sql
@@ -16,11 +16,23 @@ JOIN courses ON educations."courseId" = courses.id
 WHERE users.id = 30
 AND educations.status = 'finished';
 
---Questão 4
-
+--Questão 4 --
 SELECT users.id, users.name, roles.name AS "role", companies.name AS "company", experiences."startDate" FROM users
 JOIN experiences ON experiences."userId" = users.id
 JOIN roles ON roles.id = experiences."roleId"
 JOIN companies ON companies.id = experiences."companyId"
 WHERE users.id = 50
 AND experiences."endDate" IS NULL;
+
+-- Questão bônus --
+SELECT schools.id, schools.name AS "school", courses.name AS "course", companies.name AS "company", roles.name AS "role" FROM jobs
+JOIN companies ON companies.id = jobs."companyId"
+JOIN roles ON roles.id = jobs."roleId"
+JOIN applicants ON applicants."jobId" = jobs.id
+JOIN users ON users.id = applicants."userId"
+JOIN educations ON educations."userId" = users.id
+JOIN courses ON courses.id = educations."courseId"
+JOIN schools ON schools.id = educations."schoolId"
+WHERE companies.id = 10
+AND jobs.active = true
+AND roles.name = 'Software Engineer';
